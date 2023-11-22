387ea011f8c56a4113b33b18068eb6fca561b58e
manchikantisaikiran <manchikantisaikiran777@gmail.com>
2023-11-22 15:54:07 +0530

    JavaScript Tic-Tac-Toe

---
+++ Week-5/DOM BOM & Storage/JavaScript Tic-Tac-Toe/tictactoe.js
@@ -0,0 +1,155 @@
+let playerTurn = true;
+let computerMoveTimeout = 0;
+let turnInfo;
+const gameStatus = {
+	MORE_MOVES_LEFT: 1,
+	HUMAN_WINS: 2,
+	COMPUTER_WINS: 3,
+	DRAW_GAME: 4
+};
+
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded() {
+	// Setup the click event for the "New game" button
+	const newBtn = document.getElementById("newGameButton");
+	newBtn.addEventListener("click", newGame);
+	turnInfo = document.querySelector('#turnInfo');
+	// Create click-event handlers for each game board button
+	const buttons = getGameBoardButtons();
+	for (let button of buttons) {
+		button.addEventListener("click", function () {
+			if(playerTurn){
+				boardButtonClicked(button);
+			}
+		});
+	}
+
+	// Clear the board
+	newGame();
+}
+
+// Returns an array of 9 <button> elements that make up the game board. The first 3 
+// elements are the top row, the next 3 the middle row, and the last 3 the 
+// bottom row. 
+function getGameBoardButtons() {
+	return document.querySelectorAll("#gameBoard > button");
+}
+
+function checkForWinner() {
+
+	const buttons = getGameBoardButtons();
+
+	// Ways to win
+	const possibilities = [
+		[0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+		[0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+		[0, 4, 8], [2, 4, 6] // diagonals
+	];
+
+	// Check for a winner first
+	for (let indices of possibilities) {
+		if (buttons[indices[0]].innerHTML !== "" &&
+			buttons[indices[0]].innerHTML === buttons[indices[1]].innerHTML &&
+			buttons[indices[1]].innerHTML === buttons[indices[2]].innerHTML) {
+
+			// Found a winner
+			if (buttons[indices[0]].innerHTML === "X") {
+				return gameStatus.HUMAN_WINS;
+			}
+			else {
+				return gameStatus.COMPUTER_WINS;
+			}
+		}
+	}
+
+	// See if any more moves are left
+	for (let button of buttons) {
+		if (button.innerHTML !== "X" && button.innerHTML !== "O") {
+			return gameStatus.MORE_MOVES_LEFT;
+		}
+	}
+
+	// If no winner and no moves left, then it's a draw
+	return gameStatus.DRAW_GAME;
+}
+
+function newGame() {
+	// TODO: Complete the function
+	if (computerMoveTimeout) {
+		clearTimeout(computerMoveTimeout);
+	}
+	computerMoveTimeout = 0;
+
+	const buttons = getGameBoardButtons();
+	for (let button of buttons) {
+		button.innerHTML = '';
+		button.disabled = false;
+		button.className = '';
+	}
+	playerTurn = true;
+	turnInfo.textContent = 'Your turn';
+}
+
+function boardButtonClicked(button) {
+	// TODO: Complete the function
+	if (playerTurn) {
+		button.textContent = 'X'
+		button.classList.add('x');
+	} else {
+		button.textContent = 'O'
+		button.classList.add('o');
+	}
+	button.disabled = true;
+	switchTurn()
+}
+
+function switchTurn() {
+	// TODO: Complete the function
+	const gameStatusLocal = checkForWinner();
+	playerTurn = !playerTurn;
+
+	if (gameStatusLocal == gameStatus.MORE_MOVES_LEFT) {
+		if (playerTurn) {
+			turnInfo.textContent = 'Your turn';
+		} else {
+			computerMoveTimeout = setTimeout(() => {
+				makeComputerMove();
+			}, 1000);
+			turnInfo.textContent = 'Computer\'s turn';
+		}
+	} else {
+		playerTurn = false;
+		if (gameStatusLocal == gameStatus.DRAW_GAME) {
+			turnInfo.textContent = 'Draw game';
+		}
+		if (gameStatusLocal == gameStatus.HUMAN_WINS) {
+			turnInfo.textContent = 'You win!';
+		}
+		if (gameStatusLocal == gameStatus.COMPUTER_WINS) {
+			turnInfo.textContent = 'Computer wins!';
+		}
+	}
+
+
+}
+
+function makeComputerMove() {
+	// TODO: Complete the function
+	const buttons = getGameBoardButtons();
+	let findEmptyBtn = true;
+	const indexMap = {};
+
+	while(findEmptyBtn){
+		const randomIndex = Math.floor(Math.random() * 9);
+		if(!indexMap[randomIndex] && buttons[randomIndex].textContent != 'X'){
+			indexMap[randomIndex] = true;
+			const btn = buttons[randomIndex];
+			btn.textContent = 'O';
+			btn.className = 'o';
+			btn.disabled = false
+			findEmptyBtn = false;
+			switchTurn();
+		}
+	}
+}
